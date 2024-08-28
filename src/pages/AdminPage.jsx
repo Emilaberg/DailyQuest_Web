4db48b3380c766f596d4ac32e95d605928b84644
EmmaRhodin <143386528+EmmaRhodin@users.noreply.github.com>
2024-08-28 13:33:41 +0200

    Created components for Tracked Emails & Available Quizzes for the Admin page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,4 +1,6 @@
 import SideBar from "../components/SideBar.jsx";
+import TrackedEmails from "../components/TrackedEmails.jsx";
+import AvailableQuizzes from "../components/AvailableQuizzes.jsx";
 
 function AdminPage() {
   return (
@@ -10,25 +12,10 @@ function AdminPage() {
         <div className="flex h-screen">
           <SideBar />
 
-          {/* Main Content Holder */}
-          <div className="text-white flex-1 w-full bg-midnightBlue p-8 ml-10 mr-10 mt-10 mb-10">
-            <div className="p-4 flex items-center justify-center">
-              <h1>Dashboard Details</h1>
-            </div>
-            <div className="text-4xl flex items-center justify-center">
-              <h1>Tracked Emails</h1>
-            </div>
-            <div>
-              <div className="border-t-2 border-b-2 border-white flex items-center justify-center mt-3">
-                <div className="flex items-center w-full h-10 rounded-lg bg-adminGray mt-3 mb-3">
-                  <p>Name & Email</p>
-                  <p>Role</p>
-                  <p>Email Sent</p>
-                  <p>Actions</p>
-                </div>
-              </div>
-              <p>Remove Selected</p>
-            </div>
+          {/* Main Content Holders */}
+          <div className="w-9/12">
+            <TrackedEmails />
+            <AvailableQuizzes />
           </div>
         </div>
       </div>
