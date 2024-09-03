4191a9ff9479f953ebf96847553723c6228d7a64
JonhannesNilsson <jonte9203@hotmail.com>
2024-09-03 18:46:08 +0200

    cleaned comments

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,14 +1,6 @@
 import QuizComponent from "../components/QuizComponent";
-import { useEffect } from "react";
-
-// function useScrollToTop() {
-//   useEffect(() => {
-//     window.scrollTo(0, 0);
-//   }, []);
-// }
 
 function GamePage() {
-  // useScrollToTop();
   return (
     <>
       <div className="min-h-screen">
