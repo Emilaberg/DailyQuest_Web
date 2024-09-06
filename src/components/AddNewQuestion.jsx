b6d214d61f69ca3818a730eb7d288d18f2480a3c
EmmaRhodin <143386528+EmmaRhodin@users.noreply.github.com>
2024-09-06 10:36:38 +0200

    Worked on AddNewQuestion UI

--- src/components/AddNewQuestion.jsx
+++ src/components/AddNewQuestion.jsx
@@ -16,7 +16,6 @@ function AddNewQuestion() {
   };
 
   const handleCheckboxChange = (index) => {
-    // Set all checkboxes to false except the one being checked
     const newPrompts = prompts.map((prompt, idx) => ({
       ...prompt,
       checked: idx === index,
@@ -79,21 +78,21 @@ function AddNewQuestion() {
           <p className="text-xs">Is This The Correct Answer?</p>
         </div>
 
-        <div className="flex items-center">
+        <div className="flex items-center relative">
           <DndContext onDragEnd={handleDragEnd}>
             <Droppable id="droppable-container">
-              <div className="w-96 mt-2 border border-lavender rounded-sm p-0.5">
+              <div className="w-96 mt-2 border-l border-t border-b border-lavender rounded-sm p-0.5 mr-12">
                 {prompts.map((prompt, index) => (
-                  <Draggable key={prompt.id} id={String(prompt.id)}>
-                    <div className="relative flex items-center w-full p-1 cursor-pointer">
+                  <Draggable key={prompt.id} id={String(prompt.id)} dragHandle>
+                    <div className="relative flex items-center w-full p-1">
                       <img
                         src={edit}
                         alt="Move"
-                        className="w-8 mr-2.5 ml-2.5"
+                        className="w-8 mr-2.5 ml-2.5 cursor-grab"
                       />
                       <input
                         type="text"
-                        className="p-2 pr-10 border border-lavender rounded-sm bg-charcoalBlack w-full"
+                        className="p-2 pr-10 border-l border-t border-b border-lavender rounded-sm bg-charcoalBlack w-full"
                         placeholder="Enter your text here"
                         value={prompt.text}
                         onChange={(e) =>
@@ -108,17 +107,19 @@ function AddNewQuestion() {
           </DndContext>
 
           {/* Checkboxes outside the draggable container */}
-          <div className="flex flex-col items-start mr-6 mt-4">
+          <div className="absolute mt-2 flex border-r border-t border-b flex-col items-start p-0.5 mr-6 border-lavender rounded-sm right-6 top-0">
             {prompts.map((prompt, index) => (
-              <div key={prompt.id} className="flex items-center mb-2">
-                <input
-                  type="checkbox"
-                  checked={prompt.checked}
-                  onChange={() => handleCheckboxChange(index)}
-                  className="appearance-none border-2 mt-3 mb-3.5 ml-2 border-white rounded-sm 
-                  w-4 h-4 bg-charcoalBlack checked:bg-white 
+              <div key={prompt.id} className="p-1">
+                <div className="flex items-center border-r border-t border-b border-lavender rounded-sm">
+                  <input
+                    type="checkbox"
+                    checked={prompt.checked}
+                    onChange={() => handleCheckboxChange(index)}
+                    className="appearance-none border-2 mt-3 mr-3 mb-3.5 ml-2 border-white rounded-sm 
+                  w-3.5 h-3.5 bg-charcoalBlack checked:bg-white 
                   checked:border-white active:bg-darkLavender"
-                />
+                  />
+                </div>
               </div>
             ))}
           </div>
