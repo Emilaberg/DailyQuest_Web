7fb393cd15a9cbae224a657d3c39573612d99bcb
JonhannesNilsson <jonte9203@hotmail.com>
2024-09-13 09:49:34 +0200

    added new page, fixed image bugs

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -2,21 +2,24 @@ import { Link } from "react-router-dom";
 
 function GameCard({ imageUrl, gameName, quizId }) {
   return (
-    <div className="w-full h-1/2">
+    <>
       <Link
         className="cursor-pointer transition-transform transform hover:scale-105 duration-300 ease-in-out border-b-4 border-transparent hover:border-primaryblue"
         to={`/games/${quizId}`}
       >
-        <div className="flex flex-col justify-end h-full">
-          <img className=" bg-center bg-cover" src={`${imageUrl}`} />
+        <div className="flex flex-col justify-end ">
+          <img
+            className="w-full h-[150px] object-cover object-center"
+            src={`${imageUrl}`}
+          />
         </div>
         <div className="flex items-center w-full justify-start py-1 bg-slateBlue lg:py-2 ">
           <h3 className="text-white font-medium text-xs pl-2 md:text-sm lg:text-base xl:text-lg ">
-            {gameName}
+            {gameName} - Quiz
           </h3>
         </div>
       </Link>
-    </div>
+    </>
   );
 }
 
